85fa74b1bd8a2cdd41944f08be7f1cff011ed358
Alexey Khatskelevich <hatskelevich.alesha@gmail.com>
2022-12-07 22:59:07 +0500

    feat: added gsap for smooth scrolling

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 window.addEventListener('scroll', _ => {
-    document.body.style.setProperty('--scrollTop', `${ window.scrollY }px`)
+    document.body.style.setProperty('--scrollTop', `${window.scrollY}px`)
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ScrollSmoother.create({
+    smooth: 1,
+    effects: true,
+    smoothTouch: 0.1,
 })
